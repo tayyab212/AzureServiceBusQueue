635a3ae18e27a7523ea6bd3f815ec05f883d7a1f
“tayyab212” <tthunjra@gmail.com>
2023-05-04 19:13:46 +0200

    initial

--- Program.cs
+++ Program.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
 
 namespace AzureServiceBusQueue
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      
+            static async System.Threading.Tasks.Task Main(string[] args)
+            {
+                string connectionString = "place your connection string here";
+                string queueName = "myqueue123";
+                // since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+                await using var client = new ServiceBusClient(connectionString);
+
+                // create the sender
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a set of messages that we can send
+                ServiceBusMessage[] messages = new ServiceBusMessage[]
+                {
+    new ServiceBusMessage("First"),
+    new ServiceBusMessage("Second"),
+    new ServiceBusMessage("Third")
+                };
+
+                // send the message batch
+                await sender.SendMessagesAsync(messages);
+
+                // create the options to use for configuring the processor
+                var options = new ServiceBusProcessorOptions
+                {
+                    // By default or when AutoCompleteMessages is set to true, the processor will complete the message after executing the message handler
+                    // Set AutoCompleteMessages to false to [settle messages](/azure/service-bus-messaging/message-transfers-locks-settlement#peeklock) on your own.
+                    // In both cases, if the message handler throws an exception without settling the message, the processor will abandon the message.
+                    AutoCompleteMessages = false,
+
+                    // I can also allow for multi-threading
+                    MaxConcurrentCalls = 2
+                };
+
+                // create a processor that we can use to process the messages
+                await using ServiceBusProcessor processor = client.CreateProcessor(queueName, options);
+
+                // configure the message and error handler to use
+                processor.ProcessMessageAsync += MessageHandler;
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                async Task MessageHandler(ProcessMessageEventArgs args)
+                {
+                    string body = args.Message.Body.ToString();
+                    Console.WriteLine(body);
+
+                    // we can evaluate application logic and use that to determine how to settle the message.
+                    await args.CompleteMessageAsync(args.Message);
+                }
+
+                Task ErrorHandler(ProcessErrorEventArgs args)
+                {
+                    // the error source tells me at what point in the processing an error occurred
+                    Console.WriteLine(args.ErrorSource);
+                    // the fully qualified namespace is available
+                    Console.WriteLine(args.FullyQualifiedNamespace);
+                    // as well as the entity path
+                    Console.WriteLine(args.EntityPath);
+                    Console.WriteLine(args.Exception.ToString());
+                    return Task.CompletedTask;
+                }
+
+                // start processing
+                await processor.StartProcessingAsync();
+
+                // since the processing happens in the background, we add a Console.ReadKey to allow the processing to continue until a key is pressed.
+                Console.ReadKey();
+            }
+        
     }
 }
